ebf53f8a895ceddccb0cdb13642cea574f48e3ec
ivankolev <ivankolev@gmail.com>
2016-02-15 18:52:26 -0500

    make it random and fix xml error

--- jokeslib/src/main/java/com/phaseshiftlab/phaseshifterbuilditbigger/lib/JokeSupplier.java
+++ jokeslib/src/main/java/com/phaseshiftlab/phaseshifterbuilditbigger/lib/JokeSupplier.java
@@ -1,19 +1,22 @@
 package com.phaseshiftlab.phaseshifterbuilditbigger.lib;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class JokeSupplier {
 
     public static String supplyJoke(){
         java.util.Properties prop = new Properties();
+        Integer jokesLength;
         try {
             prop.loadFromXML(JokeSupplier.class.getResourceAsStream("/jokes.xml"));
+            jokesLength = prop.size() -1;
+            int rand = ThreadLocalRandom.current().nextInt(0,jokesLength);
+            return prop.getProperty("joke" + rand);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(prop.getProperty("joke1"));
-        return prop.getProperty("joke1");
+        return "No Jokes!";
     }
 }
